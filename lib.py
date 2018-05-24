dd8bf09e44c3f5cf744ae27717635a2e959459c9
Celeo <celeodor@gmail.com>
2018-05-23 18:27:10 -0700

    Mail support

--- lib.py
+++ lib.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import sqlite3
 
@@ -28,7 +29,7 @@ class CharacterExplorer:
         self.redirect_uri = redirect_uri
         self.refresh_token = refresh_token
         self.sde_path = sde_path or 'sqlite-latest.sqlite'
-        self.data: dict = {}
+        self.char_data: dict = {}
         self._verify_sde()
         self._setup_esi()
 
@@ -71,7 +72,7 @@ class CharacterExplorer:
             'refresh_token': self.refresh_token
         })
         self.security.refresh()
-        self.data['verify'] = self.security.verify()
+        self.char_data = self.security.verify()
 
     def _do_sde_query(self, query, *args) -> list:
         """Runs a query against the SDE data.
@@ -100,7 +101,7 @@ class CharacterExplorer:
         Returns:
             name
         """
-        return self.data['verify']['CharacterName']
+        return self.char_data['CharacterName']
 
     def get_character_id(self) -> int:
         """Returns the character's id
@@ -111,7 +112,7 @@ class CharacterExplorer:
         Returns:
             id
         """
-        return self.data['verify']['CharacterID']
+        return self.char_data['CharacterID']
 
     def get_assets(self) -> list:
         """Returns the character's assets.
@@ -122,8 +123,6 @@ class CharacterExplorer:
         Returns:
             list of assets
         """
-        if 'assets' in self.data:
-            return self.data['assets']
         op = self.app.op['get_characters_character_id_assets'](character_id=self.get_character_id())
         data = self.client.request(op).data
         ids = [item['type_id'] for item in data]
@@ -133,7 +132,6 @@ class CharacterExplorer:
             item_lookups[pair[0]] = pair[1]
         for item in data:
             item['type_name'] = item_lookups[item['type_id']]
-        self.data['assets'] = data
         return data
 
     def get_corporation_history(self) -> list:
@@ -145,19 +143,16 @@ class CharacterExplorer:
         Returns:
             list of corporation history entries
         """
-        if 'corp_history' in self.data:
-            return self.data['corp_history']
         op = self.app.op['get_characters_character_id_corporationhistory'](character_id=self.get_character_id())
         data = self.client.request(op).data
         ids = [item['corporation_id'] for item in data]
-        op = self.app.op['post_universe_names'](ids=ids)
+        op = self.app.op['post_universe_names'](ids=set(ids))
         ids_data = self.client.request(op).data
         ids_lookup = {}
         for entry in ids_data:
             ids_lookup[entry['id']] = entry['name']
         for item in data:
             item['corporation_name'] = ids_lookup[item['corporation_id']]
-        self.data['corp_history'] = data
         return data
 
     def get_contacts(self) -> list:
@@ -169,19 +164,16 @@ class CharacterExplorer:
         Returns:
             list of contacts
         """
-        if 'contacts' in self.data:
-            return self.data['contacts']
         op = self.app.op['get_characters_character_id_contacts'](character_id=self.get_character_id())
         data = self.client.request(op).data
         ids = [item['contact_id'] for item in data]
-        op = self.app.op['post_universe_names'](ids=ids)
+        op = self.app.op['post_universe_names'](ids=set(ids))
         ids_data = self.client.request(op).data
         ids_lookup = {}
         for entry in ids_data:
             ids_lookup[entry['id']] = entry['name']
         for item in data:
             item['contact_name'] = ids_lookup[item['contact_id']]
-        self.data['contacts'] = data
         return data
 
     def get_wallet_balance(self) -> float:
@@ -193,11 +185,8 @@ class CharacterExplorer:
         Returns:
             wallet balance
         """
-        if 'wallet' in self.data:
-            return self.data['wallet']
         op = self.app.op['get_characters_character_id_wallet'](character_id=self.get_character_id())
         data = self.client.request(op).data
-        self.data['wallet'] = data
         return data
 
     def get_wallet_journal(self) -> list:
@@ -212,23 +201,88 @@ class CharacterExplorer:
         Returns:
             list of wallet journal entries
         """
-        if 'wallet_journal' in self.data:
-            return self.data['wallet_journal']
         op = self.app.op['get_characters_character_id_wallet_journal'](character_id=self.get_character_id(), page=1)
         data = self.client.request(op).data
         ids = []
         for item in data:
             ids.append(item['first_party_id'])
             ids.append(item['second_party_id'])
-        ids_data = self.client.request(self.app.op['post_universe_names'](ids=ids)).data
+        ids_data = self.client.request(self.app.op['post_universe_names'](ids=set(ids))).data
         ids_lookup = {}
         for entry in ids_data:
             ids_lookup[entry['id']] = entry['name']
         for item in data:
             item['first_party_name'] = ids_lookup[item['first_party_id']]
             item['second_party_name'] = ids_lookup[item['second_party_id']]
-        self.dawta['wallet_journal'] = data
         return data
+
+    def _convert_swagger_dt(self, dt: 'pyswagger.primitives._time.Datetime') -> datetime.datetime:
+        """Converts a pyswagger timestamp.
+
+        Args:
+            dt: pyswagger timestamp
+
+        Returns:
+            Python stdlib datetime object
+        """
+        return datetime.datetime.strptime(dt.to_json(), '%Y-%m-%dT%H:%M:%S+00:00')
+
+    def get_mail_headers(self, back_until: datetime.datetime = None) -> list:
+        """Returns the character's mail headers.
+
+        This method keeps calling ESI, going backuntil the passed time (default 6 months).
+
+        Args:
+            back_util: how far back to retrieve mail
+
+        Returns:
+            list of mail headers
+        """
+        back_until = back_until or datetime.datetime.utcnow() - datetime.timedelta(days=30 * 6)
+        data = []
+        op = self.app.op['get_characters_character_id_mail'](character_id=self.get_character_id())
+        data.extend(self.client.request(op).data)
+        while True:
+            if self._convert_swagger_dt(data[-1]['timestamp']) < back_until:
+                break
+            op = self.app.op['get_characters_character_id_mail'](character_id=self.get_character_id(), last_mail_id=data[-1]['mail_id'])
+            new_data = self.client.request(op).data
+            if not new_data:
+                break
+            data.extend(new_data)
+        all_data = data.copy()
+        data.clear()
+        for item in all_data:
+            if self._convert_swagger_dt(item['timestamp']) < back_until:
+                break
+            data.append(item)
+        ids = []
+        for item in data:
+            ids.append(item['from'])
+            for recip in item['recipients']:
+                ids.append(recip['recipient_id'])
+        ids_data = self.client.request(self.app.op['post_universe_names'](ids=set(ids))).data
+        ids_lookup = {}
+        for entry in ids_data:
+            ids_lookup[entry['id']] = entry['name']
+        for item in data:
+            item['from_name'] = ids_lookup[item['from']]
+            for recip in item['recipients']:
+                recip['recipient_name'] = ids_lookup[recip['recipient_id']]
+        return data
+
+    def get_mail_body(self, mail_id: int) -> str:
+        """Returns a specific mail's body.
+
+        Args:
+            mail_id: id of the mail item
+
+        Returns:
+            mail body
+        """
+        return self.client.request(self.app.op['get_characters_character_id_mail_mail_id'](
+            character_id=self.get_character_id(), mail_id=mail_id)
+        ).data['body']
 
 
 all_esi_scopes: list = [
