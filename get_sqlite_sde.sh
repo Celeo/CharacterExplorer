d86338ea03d5d8f8f3843309306c9325c5ae7abd
Celeo <celeodor@gmail.com>
2018-05-25 20:54:21 -0700

    Fixed typo

--- get_sqlite_sde.sh
+++ get_sqlite_sde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Getting file from fuzzwork"
 wget https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2
-echo "Uncompressing"
+echo "Decompressing"
 bzip2 -d sqlite-latest.sqlite.bz2
 mv sqlite-latest.sqlite sde.db
 echo "Done"
